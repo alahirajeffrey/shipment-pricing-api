694777692458e9cf275d03685326bec058bc471e
Jeffrey Alahira <alahirajeffrey@gmail.com>
2025-02-04 22:38:56 +0100

    updated server security

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import cors from "cors";
 import express from "express";
+import { rateLimiter } from "./common";
+import helmet from "helmet";
 
 const app = express();
 
-app.use(cors());
+app.use(helmet());
+app.use(cors({ origin: "*" }));
+app.use(rateLimiter);
 app.use(express.json());
 
 export default app;

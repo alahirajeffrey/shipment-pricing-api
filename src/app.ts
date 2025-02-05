2fd600a9d7e9f99097838de933a754be14fc491e
Jeffrey Alahira <alahirajeffrey@gmail.com>
2025-02-05 09:16:45 +0100

    code refactor

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,18 @@ import mongoose from "mongoose";
 import { config } from "./config";
 import app from "./server";
 
-async () => {
-  mongoose
-    .connect(config.MONGO_URI)
-    .then(() => {
-      "Mongodb connected";
-    })
-    .catch((error) => {
-      console.error("MongoDB connection error:", error);
-      process.exit(1);
-    });
-};
+// Connect to MongoDB
+mongoose
+  .connect(config.MONGO_URI, {})
+  .then(() => {
+    console.log("Connected to MongoDB");
 
-app.listen(config.PORT, () => {
-  console.log(`Server running on http://localhost:${config.PORT}`);
-});
+    // Start the server only after successful connection
+    app.listen(config.PORT, () => {
+      console.log(`Server running on http://localhost:${config.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });

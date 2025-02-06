97bf96378434495a9c57554ab61d9b3894842c0f
Jeffrey Alahira <alahirajeffrey@gmail.com>
2025-02-06 03:04:30 +0100

    setup routes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import { rateLimiter } from "./common";
 import helmet from "helmet";
+import { router } from "./routes";
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(helmet());
 app.use(cors({ origin: "*" }));
 app.use(rateLimiter);
 app.use(express.json());
+
+app.use(router);
 
 export default app;

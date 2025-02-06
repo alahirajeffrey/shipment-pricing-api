2fd40f30a9fac6d066c107d7c5464fb8eb454751
Jeffrey Alahira <alahirajeffrey@gmail.com>
2025-02-06 03:24:41 +0100

    added validation for route to calc shipping costs

--- src/common/routes.validations.ts
+++ src/common/routes.validations.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { ShipmentEnum } from "./shipment.enum";
 
 export const addPricingValidation = z.object({
   type: z.string().min(1, { message: "Type is required" }),
@@ -7,4 +8,14 @@ export const addPricingValidation = z.object({
 
 export const getPricingValidation = z.object({
   id: z.string().length(24, { message: "Invalid ID format" }), // MongoDB ObjectId is 24 characters
+});
+
+export const calculateShippingValidation = z.object({
+  weight: z.number().min(0.1, "Weight must be greater than 0"),
+  distance: z.number().min(1, "Distance must be at least 1"),
+  type: z.enum([ShipmentEnum.GENERAL, ShipmentEnum.FRAGILE], {
+    errorMap: () => ({
+      message: "Invalid cargo type. Allowed: general, fragile.",
+    }),
+  }),
 });

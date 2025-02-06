dc83ef57991e9a2b2c2fc33e6c18ad769f8648fc
Jeffrey Alahira <alahirajeffrey@gmail.com>
2025-02-06 10:36:53 +0100

    commit

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -11,12 +11,12 @@ import { calculateShippingCost } from "../controllers/shipment.controller";
 
 const router = express.Router();
 
-router.post("pricing", addPricing);
+router.post("/pricing", addPricing);
+router.get("/pricing/type/:type", getPricingByType);
 router.get("/pricing/:id", getPricingById);
-router.get("/pricing/:type", getPricingByType);
 router.get("/pricing", getAllPricing);
 router.patch("/pricing/:id", updatePricing);
-router.delete("/pricing", deletePricing);
-router.post("/shipping/calculate", calculateShippingCost);
+router.delete("/pricing/:id", deletePricing);
+router.post("/shipment/calculate", calculateShippingCost);
 
 export { router };

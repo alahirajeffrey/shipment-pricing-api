31e3c29baba89ea9ef5d8054cb2780aa4d203936
Jeffrey Alahira <alahirajeffrey@gmail.com>
2025-02-06 10:12:05 +0100

    added validation

--- src/common/routes.validations.ts
+++ src/common/routes.validations.ts
@@ -2,12 +2,24 @@ import { z } from "zod";
 import { ShipmentEnum } from "./shipment.enum";
 
 export const addPricingValidation = z.object({
-  type: z.string().min(1, { message: "Type is required" }),
+  type: z.enum([ShipmentEnum.GENERAL, ShipmentEnum.FRAGILE], {
+    errorMap: () => ({
+      message: "Invalid cargo type. Allowed: general, fragile.",
+    }),
+  }),
   price: z.number().positive({ message: "Price must be a positive number" }),
 });
 
 export const getPricingValidation = z.object({
-  id: z.string().length(24, { message: "Invalid ID format" }), // MongoDB ObjectId is 24 characters
+  id: z.string().length(24, { message: "Invalid ID format" }),
+});
+
+export const getPricingByTypeValidation = z.object({
+  type: z.enum([ShipmentEnum.GENERAL, ShipmentEnum.FRAGILE], {
+    errorMap: () => ({
+      message: "Invalid cargo type. Allowed: general, fragile.",
+    }),
+  }),
 });
 
 export const calculateShippingValidation = z.object({
